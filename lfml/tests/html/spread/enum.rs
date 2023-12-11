65381638abc2750db4721abef7fb8dce079a073b
Ben Lewington <ben.lewington91@ntlworld.com>
2023-12-11 12:47:53 +0000

    fixed up the tags internal attrs parsing

--- lfml/tests/html/spread/enum.rs
+++ lfml/tests/html/spread/enum.rs
@@ -1,3 +1,4 @@
+use crate::assert_html_eq;
 use lfml_macros::Spread;
 
 #[test]
@@ -8,4 +9,16 @@ fn basic() {
         Get { get: &'a str },
         Post { post: &'a str },
     }
+
+    let x = HxControl::Get { get: "/" };
+
+    assert_html_eq!({
+        a@(x) { "A" }
+    } => "<a hx-get=\"/\">A</a>");
+
+    let x = HxControl::Post { post: "/" };
+
+    assert_html_eq!({
+        a@(x) { "A" }
+    } => "<a hx-post=\"/\">A</a>");
 }
